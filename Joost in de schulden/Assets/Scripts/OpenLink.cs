3c4663b07dc6ae84097f58ec30419c0b1f093dc3
DennisWolff <114189594+DennisWolff@users.noreply.github.com>
2023-01-14 16:46:24 +0100

    Quiz Script/Updates
    Dingen die ik heb gedaan:
    - Achtergrond op de quiz gedaan.
    - Aangeving van wat goed en fout is met groen en rood.
    - Sound toegevoegd aan de buttons als je erop drukt.
    - WaitingTime van de vragen neergezet en dat het reset zodat de kleur niet blijft.

---
+++ Joost in de schulden/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+  
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://gemeente.groningen.nl/");
+    }
+    
+}
